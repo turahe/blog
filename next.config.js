11648d9156a14e08a07da21a431016b7f598fa84
Nur Wachid <nur@wach.id>
2023-09-13 20:21:37 +0800

    update next config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 const { withContentlayer } = require('next-contentlayer')
 const withPWA = require('next-pwa')({
   dest: 'public',
-  // disable: process.env.NODE_ENV === 'development',
+  disable: process.env.NODE_ENV === 'development',
   register: true,
   sw: 'service-worker.js',
 })
