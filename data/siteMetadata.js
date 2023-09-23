ff8db4bdb7b8b6029ac9c153a9d7f6c63c3060e9
Nur Wachid <nur@wach.id>
2023-09-23 22:54:11 +0800

    change analytics

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -25,12 +25,12 @@ const siteMetadata = {
     // supports Plausible, Simple Analytics, Umami, Posthog or Google Analytics.
     umamiAnalytics: {
       // We use an env variable for this site to avoid other users cloning our analytics ID
-      umamiWebsiteId: process.env.NEXT_UMAMI_ID, // e.g. 123e4567-e89b-12d3-a456-426614174000
-      src: process.env.NEXT_UMAMI_SRC,
+      // umamiWebsiteId: process.env.NEXT_UMAMI_ID, // e.g. 123e4567-e89b-12d3-a456-426614174000
+      // src: process.env.NEXT_UMAMI_SRC,
     },
-    // plausibleAnalytics: {
-    //   plausibleDataDomain: '', // e.g. tailwind-nextjs-starter-blog.vercel.app
-    // },
+    plausibleAnalytics: {
+      plausibleDataDomain: 'blog-turahe.vercel.app', // e.g. tailwind-nextjs-starter-blog.vercel.app
+    },
     // simpleAnalytics: {},
     // posthogAnalytics: {
     //   posthogProjectApiKey: '', // e.g. 123e4567-e89b-12d3-a456-426614174000
