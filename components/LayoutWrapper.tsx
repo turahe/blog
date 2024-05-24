88312f30eb2ba148003c4f60885ee03a4db93ebc
Nur Wachid <nur@wach.id>
2024-05-24 23:49:43 +0700

    add vercel anaytics

--- components/LayoutWrapper.tsx
+++ components/LayoutWrapper.tsx
@@ -3,7 +3,7 @@ import SectionContainer from './SectionContainer'
 import Footer from './Footer'
 import { ReactNode } from 'react'
 import Header from './Header'
-
+import { Analytics } from "@vercel/analytics/react"
 interface Props {
   children: ReactNode
 }
@@ -15,6 +15,7 @@ const inter = Inter({
 const LayoutWrapper = ({ children }: Props) => {
   return (
     <SectionContainer>
+      <Analytics/>
       <div className={`${inter.className} flex h-screen flex-col justify-between font-sans`}>
         <Header />
         <main className="mb-auto">{children}</main>
