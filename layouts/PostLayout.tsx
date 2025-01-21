af472fbc3fb7349718bd227bc2452432668e40e5
Nur Wachid <wachid@outlook.com>
2025-01-21 21:03:32 +0700

    remove react share

--- layouts/PostLayout.tsx
+++ layouts/PostLayout.tsx
@@ -9,15 +9,7 @@ import Image from '@/components/Image'
 import Tag from '@/components/Tag'
 import siteMetadata from '@/data/siteMetadata'
 import ScrollTopAndComment from '@/components/ScrollTopAndComment'
-import {
-  FacebookShareButton,
-  TwitterShareButton,
-  EmailShareButton,
-  LinkedinShareButton,
-  RedditShareButton,
-  WhatsappShareButton,
-} from 'react-share'
-import { SocialIcon } from 'react-social-icons'
+
 const editUrl = (path) => `${siteMetadata.siteRepo}/blob/main/data/${path}`
 const discussUrl = (path) =>
   `https://mobile.twitter.com/search?q=${encodeURIComponent(`${siteMetadata.siteUrl}/${path}`)}`
