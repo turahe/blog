4cf6dd7b875813d4648697b3094af596ed1690ef
Nur Wachid <wachid@outlook.com>
2025-01-21 20:38:25 +0700

    change framer motion to motion react

--- components/ThemeSwitch.tsx
+++ components/ThemeSwitch.tsx
@@ -2,7 +2,7 @@
 
 import { useEffect, useState } from 'react'
 import { useTheme } from 'next-themes'
-import { motion } from "motion/react"
+import { motion } from 'motion/react'
 import useSound from 'use-sound'
 
 const ThemeSwitch = () => {
